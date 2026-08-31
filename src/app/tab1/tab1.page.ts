19299443bf745872b26226380fd981dedc92826a
DeveloperTDF <appdeveloper230379@gmail.com>
2026-08-31 17:13:38 -0300

    primero commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular';
+import { IonHeader, IonToolbar, IonTitle,
+   IonContent, IonCard,IonCardContent, IonCardHeader,
+   IonCardSubtitle, IonCardTitle,IonItem,
+  IonLabel,
+  IonList, } from '@ionic/angular';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonCard,
+    IonCardContent,
+    IonCardHeader,
+    IonCardSubtitle,
+    IonCardTitle,
+    IonList,
+    IonLabel,
+    IonItem,
+    // ExploreContainerComponent
+  ],
 })
 export class Tab1Page {
   constructor() {}
