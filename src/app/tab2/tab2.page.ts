675c67b554db1e7f168f19cecb929db22d9c6dd4
DeveloperTDF <appdeveloper230379@gmail.com>
2026-09-01 10:23:21 -0300

    tab 2

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard,IonCardContent, IonCardHeader,
+   IonCardSubtitle, IonCardTitle,IonItem,
+  IonLabel,
+  IonList, } from '@ionic/angular';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, 
+     IonHeader,
+        IonToolbar,
+        IonTitle,
+        IonContent,
+        IonCard,
+        IonCardContent,
+        IonCardHeader,
+       
+        IonCardTitle,
+       
+       
+  ]
 })
 export class Tab2Page {
 
